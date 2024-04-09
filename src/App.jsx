41a38032ce43493d46b94574be770364cf5c9d99
Emilio <emilio.pg95@gmail.com>
2024-04-08 20:15:00 -0600

    🚧 Add @heroicons/react dependency and update Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import Navbar from './components/Navbar';
+import Logo from './images/reddit-logo.png';
+import {SearchIcon} from '@heroicons/react/solid';
+
 
 function App() {
 
   return (
     <div>
-      <Navbar />
-      <header className="text-red-500">Hello there</header>
+      <header className="flex w-full bg-reddit_dark p-2">
+        <div className="mx-4">
+          <img src={Logo} alt="Reddit Logo" className="w-8 h-8 "/>
+        </div>
+        <form action=''>
+          <SearchIcon className="text-white"/>
+          <input type='text' className='bg-gray-800 h-8'/>
+        </form>
+      </header>
     </div>
       
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        reddit_dark: {
+          DEFAULT: '#030303',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
