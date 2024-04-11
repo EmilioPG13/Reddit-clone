7d8b8b9f0f141bbd1829df7345888b3c33cef857
Emilio <emilio.pg95@gmail.com>
2024-04-11 17:40:42 -0600

    🚀 Add PostBody component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import BoardHeader from "./components/BoardHeader.jsx"
 import Header from "./components/Header.jsx"
 import PostForm from "./components/PostForm.jsx"
+import PostBody from "./components/PostBody.jsx"
 
 function App() {
 
@@ -9,36 +10,10 @@ function App() {
       <Header />
       <BoardHeader />
       <PostForm />
+      <PostBody />
 
-      <div className="px-6 bg-reddit_dark text-reddit_text ">
-        <div className="border border-reddit_border bg-reddit_dark-brighter p-2 rounded-md">
-          <h5 className="text-reddit_text-darkest text-sm mb-1">Posted by u/AutoModerator • 10 days ago</h5>
-          <h2 className="text-xl mb-3">Monthly Getting Started / Web Dev Career Thread</h2>
-          <div className="text-reddit_text-darker text-sm leading-6</p>">
-            <p className="mb-4">Due to a growing influx of questions on this topic, it has been decided to commit a monthly thread dedicated to this topic to reduce the number of repeat posts on this topic. These types of posts will no longer be allowed in the main thread.</p>
 
-            <p className="mb-4">Many of these questions are also addressed in the sub FAQ or may have been asked in previous monthly career threads.</p>
-
-            <p className="mb-4">Subs dedicated to these types of questions include r/cscareerquestions for general and opened ended career questions and r/learnprogramming for early learning questions.</p>
-            <p className="mb-4">A general recommendation of topics to learn to become industry ready include:</p>
-
-            <ul className="list-disc list-inside mb-4">
-              <li>HTML/CSS/JS Bootcamp</li>
-              <li>Version control</li>
-              <li>Automation</li>
-              <li>Front End Frameworks (React/Vue/Etc)</li>
-              <li>APIs and CRUD</li>
-              <li>Testing (Unit and Integration)</li>
-              <li>Common Design Patterns</li>
-            </ul>
-
-            <p className="mb-4">You will also need a portfolio of work with 4-5 personal projects you built, and a resume/CV to apply for work.</p>
-
-            <p className="mb-4">Plan for 6-12 months of self study and project production for your portfolio before applying for work.</p>
-          </div>
-        </div>
-      </div>
-
+      
     </div>
 
   )
