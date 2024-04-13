d743c13a0e0b522ee4d24d3fc38c3687be330ebd
Emilio <emilio.pg95@gmail.com>
2024-04-12 20:27:28 -0600

    🚀 Update PostBody component to include upvote and downvote buttons, and improve layout

--- src/components/PostBody.jsx
+++ src/components/PostBody.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getTopPosts } from '../api/reddit';
-import { ChatAlt2Icon, ShareIcon } from '@heroicons/react/outline';
+import { ChatAlt2Icon, ShareIcon, ArrowUpIcon, ArrowDownIcon  } from '@heroicons/react/outline';
 
 function PostBody() {
     const [posts, setPosts] = useState([]);
@@ -18,26 +18,38 @@ function PostBody() {
 
     return (
         <div className="px-6 bg-reddit_dark text-reddit_text ">
-            {posts.map(post => (
-                <div key={post.data.id} className="border border-reddit_border bg-reddit_dark-brighter p-2 rounded-md">
-                    <h5 className="text-reddit_text-darkest text-sm mb-1">Posted by {post.data.author} • {post.data.created_utc}</h5>
-                    <h2 className="text-xl mb-3">{post.data.title}</h2>
-                    <div className="text-reddit_text-darker text-sm leading-6">
-                        <p className="mb-4">{post.data.selftext}</p>
+        {posts.map(post => (
+            <div key={post.data.id} className="border border-reddit_border bg-reddit_dark-brighter p-2 rounded-md mb-3">
+                <div className="flex">
+                    <div className='sidebar flex flex-col items-center justify-start bg-reddit_dark-brighter p-2 mr-2 border-r border-reddit_border'>
+                        <button className=''>
+                            <ArrowUpIcon className="w-5 h-5 text-reddit_text-darkest" />
+                        </button>
+                        <p className='my-'>{post.data.ups}</p>
+                        <button>
+                            <ArrowDownIcon className='w-5 h-5 text-reddit_text-darkest' />
+                        </button>
                     </div>
-                    <div className="flex justify-items-start px-6 py-4 bg-reddit_dark-brighter border-t border-reddit_border">
-                        <button className="mr-4 flex items-center text-reddit_text-darkest">
-                            <ChatAlt2Icon className="w-5 h-5 mr-1" />
-                            <p>{post.data.num_comments} Comments</p>
-                        </button>
-                        <button className="flex items-center text-reddit_text-darkest">
-                            <ShareIcon className="w-5 h-5 mr-1" />
-                            <p>Share</p>
-                        </button>
+                    <div>
+                        <h2 className="text-xl mb-3">{post.data.title}</h2>
+                        <div className="text-reddit_text-darker text-sm leading-6">
+                            <p className="mb-4">{post.data.selftext}</p>
+                        </div>
+                        <div className="flex justify-items-start py-1 bg-reddit_dark-brighter border-t border-reddit_border">
+                            <button className="mr-4 flex items-center text-reddit_text-darkest">
+                                <ChatAlt2Icon className="w-5 h- mr-1" />
+                                <p>{post.data.num_comments} Comments</p>
+                            </button>
+                            <button className="flex items-center text-reddit_text-darkest">
+                                <ShareIcon className="w-5 h-5 mr-1" />
+                                <p>Share</p>
+                            </button>
+                        </div>
                     </div>
                 </div>
-            ))}
-        </div>
+            </div>
+        ))}
+    </div>
     );
 }
 
