3d6ba8a4173d4408398a6f8493d7ba9de3d63847
Emilio <emilio.pg95@gmail.com>
2024-04-12 19:36:26 -0600

    🚀 Update PostBody component to fetch and display top posts from Reddit API, and add icons for comments and share buttons

--- src/components/PostBody.jsx
+++ src/components/PostBody.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { getTopPosts } from '../api/reddit';
+import { ChatAlt2Icon, ShareIcon } from '@heroicons/react/outline';
 
 function PostBody() {
     const [posts, setPosts] = useState([]);
@@ -7,44 +8,37 @@ function PostBody() {
     useEffect(() => {
         getTopPosts()
             .then(response => {
-                setPosts(response.data.data.children);
+                console.log(response);
+                setPosts(response.data.children);
             })
             .catch(error => {
                 console.error('Error fetching data from Reddit API', error);
-        });
+            });
     }, []);
 
     return (
         <div className="px-6 bg-reddit_dark text-reddit_text ">
-            
-            <div className="border border-reddit_border bg-reddit_dark-brighter p-2 rounded-md">
-                <h5 className="text-reddit_text-darkest text-sm mb-1">Posted by u/AutoModerator • 10 days ago</h5>
-                <h2 className="text-xl mb-3">Monthly Getting Started / Web Dev Career Thread</h2>
-                <div className="text-reddit_text-darker text-sm leading-6</p>">
-                    <p className="mb-4">Due to a growing influx of questions on this topic, it has been decided to commit a monthly thread dedicated to this topic to reduce the number of repeat posts on this topic. These types of posts will no longer be allowed in the main thread.</p>
-
-                    <p className="mb-4">Many of these questions are also addressed in the sub FAQ or may have been asked in previous monthly career threads.</p>
-
-                    <p className="mb-4">Subs dedicated to these types of questions include r/cscareerquestions for general and opened ended career questions and r/learnprogramming for early learning questions.</p>
-                    <p className="mb-4">A general recommendation of topics to learn to become industry ready include:</p>
-
-                    <ul className="list-disc list-inside mb-4">
-                        <li>HTML/CSS/JS Bootcamp</li>
-                        <li>Version control</li>
-                        <li>Automation</li>
-                        <li>Front End Frameworks (React/Vue/Etc)</li>
-                        <li>APIs and CRUD</li>
-                        <li>Testing (Unit and Integration)</li>
-                        <li>Common Design Patterns</li>
-                    </ul>
-
-                    <p className="mb-4">You will also need a portfolio of work with 4-5 personal projects you built, and a resume/CV to apply for work.</p>
-
-                    <p className="mb-4">Plan for 6-12 months of self study and project production for your portfolio before applying for work.</p>
+            {posts.map(post => (
+                <div key={post.data.id} className="border border-reddit_border bg-reddit_dark-brighter p-2 rounded-md">
+                    <h5 className="text-reddit_text-darkest text-sm mb-1">Posted by {post.data.author} • {post.data.created_utc}</h5>
+                    <h2 className="text-xl mb-3">{post.data.title}</h2>
+                    <div className="text-reddit_text-darker text-sm leading-6">
+                        <p className="mb-4">{post.data.selftext}</p>
+                    </div>
+                    <div className="flex justify-items-start px-6 py-4 bg-reddit_dark-brighter border-t border-reddit_border">
+                        <button className="mr-4 flex items-center text-reddit_text-darkest">
+                            <ChatAlt2Icon className="w-5 h-5 mr-1" />
+                            <p>{post.data.num_comments} Comments</p>
+                        </button>
+                        <button className="flex items-center text-reddit_text-darkest">
+                            <ShareIcon className="w-5 h-5 mr-1" />
+                            <p>Share</p>
+                        </button>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
-    )
+    );
 }
 
 export default PostBody;
