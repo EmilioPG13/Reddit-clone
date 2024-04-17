3e49f7201ab2900aed0bd25b65ac9c84b9360e3c
Emilio <emilio.pg95@gmail.com>
2024-04-16 22:03:03 -0600

    🚧 Refactor BoardHeader and Header components to update styles and improve layout / Responsiveness missing

--- src/components/BoardHeader.jsx
+++ src/components/BoardHeader.jsx
@@ -2,19 +2,19 @@ function BoardHeader() {
     return (
         <>
             {/* Banner */}
-            <div className="h-40 bg-contain mx-80 rounded-lg" style={{ backgroundImage: 'url("https://styles.redditmedia.com/t5_2qs0q/styles/bannerBackgroundImage_7glcgg5ymxp21.png")' }}>
+            <div className="h-40 bg-cover bg-no-repeat mx-80 rounded-lg" style={{ backgroundImage: 'url("https://styles.redditmedia.com/t5_2qs0q/styles/bannerBackgroundImage_7glcgg5ymxp21.png")' }}>
             </div>
             {/* Board Header */}
-            <div className="bg-reddit_dark-brighter">
+            <div className="bg-reddit_dark-bright">
                 {/* Header Content */}
-                <div className="mx-6 relative flex">
+                <div className="ml-80 relative flex">
                     {/* Board Icon */}
                     <div className="h-20 w-20 rounded-full overflow-hidden relative -top-4 border-4 border-white bg-white">
                         <img src="https://styles.redditmedia.com/t5_2qs0q/styles/communityIcon_kxcmzy9bt1381.jpg?format=pjpg&s=16025192cd7824a5f93aaa0ed9eb4f149761e18e"></img>
                     </div>
                     {/* Board Title */}
                     <div className="pt-5 pl-4 font-semibold">
-                        <h1 className="text-gray-400 text-4xl">r/webdev</h1>
+                        <h1 className="text-reddit_text text-4xl">r/webdev</h1>
                     </div>
                 </div>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
                 <div className='flex'>
                     {/* This is a button with a menu icon */}
                     <button className='px-2 py-1'>
-                        <MenuIcon className='text-gray-400 w-6 h-6 ml-[-4px]' />
+                        <MenuIcon className='text-gray-600 w-6 h-6 ml-[-4px]' />
                     </button>
                     {/* This is the Reddit logo */}
                     <img src={Logo} alt="Reddit Logo" className="w-8 h-8 mr-3 " />
@@ -19,28 +19,28 @@ function Header() {
                 </div>
                 {/* This is the search bar */}
                 <form action='' className='bg-reddit_dark-brighter px-3 flex rounded-full border border-reddit_border absolute left-1/2 transform -translate-x-1/2 w-1/2'>
-                    <SearchIcon className="text-gray-300 h-6 w-6 mt-1" />
+                    <SearchIcon className="text-gray-500 h-6 w-6 mt-1" />
                     <input type='text' className='bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white h-9' placeholder='Search' />
                 </form>
                 {/* This is the right part of the header */} 
                 <div className='flex'>
                     {/* These are various buttons with icons */} 
                     <button className='px-2 py-1 '>
-                        <ChatIcon className='text-gray-400 w-6 h-6 mx-2' />
+                        <ChatIcon className='text-gray-600 w-6 h-6 mx-2' />
                     </button>
                     <button className='px-2 py-1 flex items-center justify-center'>
-                        <PlusIcon className='text-gray-400 w-6 h-6 mx-2' />
+                        <PlusIcon className='text-gray-600 w-6 h-6 mx-2' />
                         <p className='text-stone-950'>Create</p>
                     </button>
                     <button className='px-2 py-1 '>
-                        <BellIcon className='text-gray-400 w-6 h-6 mx-2' />
+                        <BellIcon className='text-gray-600 w-6 h-6 mx-2' />
                     </button>
                     {/* This is a button with an avatar and a dropdown icon */} 
                     <button className='ml-4 flex'>
                         <div className='w-8 h-8 bg-gray-600 rounded-md'>
                             <img src={Avatar} alt="Avatar" className="block rounded-md" />
                         </div>
-                        <ChevronDownIcon className="text-gray-500 w-5 h-5 mt-2 ml-2" />
+                        <ChevronDownIcon className="text-gray-600 w-5 h-5 mt-2 ml-2" />
                     </button>
                 </div>
             </div>
