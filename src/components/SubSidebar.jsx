c96709571b0263a7a76d76b440f317376816f479
Emilio <emilio.pg95@gmail.com>
2024-04-21 18:52:47 -0600

    🚀 Refactor SubSidebar component to make the sidebar scroll with the user and add scrolling effect

--- src/components/SubSidebar.jsx
+++ src/components/SubSidebar.jsx
@@ -21,21 +21,23 @@ function SubSidebar() {
     }, []);
 
     return (
-        <div className="bg-reddit_dark-default sticky top-0">
+        <div className="bg-reddit_dark-default sticky top-0 h-screen">
             <aside className="w-72 flex-none mr-6 p-4 border border-reddit_border bg-reddit_dark-brighter rounded-md mb-3">
                 <h2 className="text-xl mb-4 text-reddit_text-default">Suggested Subreddits</h2>
-                <ul>
-                    {subredditData.map(({ subreddit, icon }) => (
-                        <li key={subreddit} className="mb-4 flex items-center">
-                            <div className='flex items-center transition duration-300 hover:bg-sky-500/75 rounded p-2 w-full'>
-                                <img src={icon} alt={subreddit} className='w-10 h-10 rounded-full mr-2' />
-                                <a href={`https://www.reddit.com/r/${subreddit}`} target="_blank" rel="noopener noreferrer" className="text-sky-700">
-                                    r/{subreddit}
-                                </a>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
+                <div className="overflow-y-auto h-[calc(100vh-10rem)]">
+                    <ul>
+                        {subredditData.map(({ subreddit, icon }) => (
+                            <li key={subreddit} className="mb-4 flex items-center">
+                                <div className='flex items-center transition duration-300 hover:bg-sky-500/75 rounded p-2 w-full'>
+                                    <img src={icon} alt={subreddit} className='w-10 h-10 rounded-full mr-2' />
+                                    <a href={`https://www.reddit.com/r/${subreddit}`} target="_blank" rel="noopener noreferrer" className="text-sky-700">
+                                        r/{subreddit}
+                                    </a>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </aside>
         </div>
     );
