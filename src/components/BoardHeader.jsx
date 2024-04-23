e01c5c6cbd0f37360ae4a87f4012f834005b5a10
Emilio <emilio.pg95@gmail.com>
2024-04-23 07:01:45 -0600

    🔧 Update BoardHeader component to use default banner and icon URLs if not provided

--- src/components/BoardHeader.jsx
+++ src/components/BoardHeader.jsx
@@ -3,20 +3,22 @@ import { useState, useEffect } from 'react';
 function BoardHeader({ subreddit = 'webdev' }) {
     const [bannerURL, setBannerUrl] = useState('');
     const [iconUrl, setIconUrl] = useState('');
+    const defaultIcon = 'https://www.redditstatic.com/icon.png';
+    const defaultBanner = 'https://www.investopedia.com/thmb/DNrU2SOx4AYYiLm9wWSJcsgzmEg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Reddit-Logo-e9537b96b55349ac8eb77830f8470c95.jpg';
 
     useEffect(() => {
         fetch(`https://www.reddit.com/r/${subreddit}/about.json`)
             .then(response => response.json())
             .then(data => {
-                setBannerUrl(data.data.banner_img);
-                setIconUrl(data.data.icon_img);
+                setBannerUrl(data.data.banner_img || defaultBanner);
+                setIconUrl(data.data.icon_img || defaultIcon);
             });
-    }, [subreddit]);
+    }, [subreddit, defaultBanner, defaultIcon]);
 
     return (
         <>
             {/* Banner */}
-            <div className="h-40 bg-cover mx-80 rounded-lg" style={{ backgroundImage: `url("${bannerURL}")`, backgroundSize: 'contain', backgroundPosition: 'center' }}>
+            <div className="h-40 bg-contain bg-repeat bg-center mx-80 rounded-lg" style={{ backgroundImage: `url("${bannerURL}")` }}>
             </div>
             {/* Board Header */}
             <div className="bg-reddit_dark-bright">
